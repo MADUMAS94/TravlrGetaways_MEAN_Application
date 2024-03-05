7f2811b91749694d9bf539efacd3495a56cc90a5
MADUMAS94 <michelle.dumas@snhu.edu>
2024-03-05 17:43:42 -0500

    Module 3 completed baseline

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,3 +1,6 @@
+var fs = require('fs');
+var trips = JSON.parse(fs.readFileSync('.data/trips.json','utf8'));
+
 /*GET TRAVEL VIEW*/
 const travel = (req, res) => {
     res.render('travel',{title: 'Travlr Gataways'});
