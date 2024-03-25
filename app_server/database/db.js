2ae8524bd359af615f65cc11862fa5ca5c3e13ab
MADUMAS94 <michelle.dumas@snhu.edu>
2024-03-25 17:46:02 -0400

    Module 4 completed baseline with seedgoose

---
+++ app_server/database/db.js
@@ -0,0 +1,78 @@
+const mongoose = require('mongoose');
+const host = process.env.DB_HOST || '127.0.0.1'; // Added from the SNHU video
+const dbURL = `mongodb://${host}/travlr`;
+const readLine = require('readline');
+
+mongoose.set('useUnifiedTopology', true);
+
+// Build the connection string and set the connection timeout.
+// timeout is in milliseconds.
+const connect = () => {
+    setTimeout(() => mongoose.connect(dbURL, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true
+    }), 1000);
+};
+
+// Monitor connection events
+mongoose.connection.on('connected', () => {
+    console.log('connected');
+});
+
+mongoose.connection.on('error', err => {
+    console.log('error: ' + err);
+    return connect();
+});
+
+mongoose.connection.on('disconnected', () => {
+    console.log('disconnected');
+});
+
+// Windows specific listner
+if (process.platform === 'win32') {
+    const rl = readLine.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+    rl.on('SIGINT', () => {
+        process.emit("SIGINT");
+    });
+}
+
+// Configure for Graceful Shutdown
+const gracefulShutdown = (msg, callback) => {
+    mongoose.connection.close(() => {
+        console.log(`Mongoose disconnected through ${msg}`);
+        callback();
+    });
+};
+
+// Event Listeners to process graceful shutdowns
+// Shutdown invoked by nodemon signal
+
+process.once('SIGUSR2', () => {
+    gracefulShutdown('nodemon restart', () => {
+        process.kill(process.pid, 'SIGUSR2');
+    });
+});
+
+// Shutdown invoked by app termination
+process.on('SIGINT', () => {
+    gracefulShutdown('app termination', () => {
+        process.exit(0);
+    });
+});
+
+// Shutdown invoked by container termination
+process.on('SIGTERM', () => {
+    gracefulShutdown('app shutdown', () => {
+        process.exit(0);
+    });
+});
+
+// Make initial connection to DB
+connect();
+
+// Import Mongoose schema
+require('/models/travlr');
